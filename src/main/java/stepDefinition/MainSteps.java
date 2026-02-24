2040dc89f53bb83a0e59254b1be5799dcb1e0158
shamili <shamilimadhu12@gmail.com>
2026-02-24 12:57:01 +0530

    mainstep changes

--- src/main/java/stepDefinition/MainSteps.java
+++ src/main/java/stepDefinition/MainSteps.java
@@ -36,8 +36,7 @@ public class MainSteps {
 		System.out.println(data.get(0));
 		System.out.println(data.get(1));
 		System.out.println(data.get(2));
-		System.out.println(data.get(3));
-
+		
 
 	  
 	}
